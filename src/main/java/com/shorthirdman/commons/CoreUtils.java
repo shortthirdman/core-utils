c27c99760b685c6eadd227f1ce15186f18ddcf67
Swetank Mohanty <swetank.mohanty@outlook.com>
2020-10-21 15:48:57 +0530

    Configure project structure

---
+++ src/main/java/com/shorthirdman/commons/CoreUtils.java
@@ -0,0 +1,12 @@
+package com.shorthirdman.commons;
+
+/**
+ * CoreUtils!
+ *
+ */
+public class CoreUtils {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/shorthirdman/commons/CoreUtilsTest.java
@@ -0,0 +1,18 @@
+package com.shorthirdman.commons;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple CoreUtils.
+ */
+public class CoreUtilsTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
